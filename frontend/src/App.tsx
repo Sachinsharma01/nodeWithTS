f19ba7550596371895186d3ee73a8dfcace5de17
Sachin Sharma <sa.sharma6@gemsol.in>
2022-08-04 12:38:53 +0530

    added frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="heading">Hello React with TS</div>
       </header>
     </div>
   );
